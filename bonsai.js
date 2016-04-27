2aff6a064a8ab59d01d430f1a5235ec9cb14a29c
Yegor Borisenco <pimmey@pimmey.com>
2016-04-27 17:30:05 +0300

    Added startup Automator script to run forever

--- bonsai.js
+++ bonsai.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var notifier = require('node-notifier');
+
 var intervalTime = 1000 * 60; // 1 minute
 var morningWatering = 10;
 var eveningWatering = 17;
@@ -9,12 +11,17 @@ var date;
 var hours;
 var minutes;
 
+notifier.notify({
+    'title': 'Bonsai watering',
+    'message': 'Time to water your plant',
+    'icon': './bonsai-icn.png'
+});
+
 setInterval(function () {
-    console.log('Running check');
     date = new Date();
     hours = date.getHours();
     minutes = date.getMinutes();
-    console.log('It\'s ' + hours + ':' + minutes);
+    //console.log('It\'s ' + hours + ':' + minutes);
 
     if ((hours === morningWatering || hours === eveningWatering) && minutes === 0) {
         notifier.notify({
