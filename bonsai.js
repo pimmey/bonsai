25748149ecd74e1b4f547aa9603122f5cf4f1b2e
Yegor Borisenco <pimmey@pimmey.com>
2016-04-28 16:20:14 +0300

    Added wait for user action

--- bonsai.js
+++ bonsai.js
@@ -27,7 +27,8 @@ setInterval(function () {
         notifier.notify({
             'title': 'Bonsai watering',
             'message': 'Time to water your plant',
-            'icon': './bonsai-icn.png'
+            'icon': './bonsai-icn.png',
+            'wait': true
         });
     }
 
